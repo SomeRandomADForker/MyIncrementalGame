294e791311954c5f37314b38cafca559b74c153e
SomeRandomADForker <13ivsmir@gmail.com>
2026-07-09 06:43:27 +0000

    Add format.js and some other changes (index.d.ts is for me as a intellisense helper)

---
+++ format.js
@@ -0,0 +1,21 @@
+/**
+ * @param {import("./index").default} dcNumber
+ */
+function format(dcNumber) {
+    // < 1.00e6 (1,000,000)
+    if (dcNumber.lt(new Decimal("1e6"))) {
+        return dcNumber.toFixed(2);
+    }
+    // > 1.00e6 (1,000,000) and < 1.00e1,000,000,000 (1.00e1.000e9)
+    else if (dcNumber.gte(new Decimal("1e6")) && dcNumber.lt(new Decimal("1e1000000000"))) {
+        return dcNumber.toExponential(2).replace(/\+/g, "");
+    }
+    // > 1.00e1.000e9
+    else {
+        let exponent = dcNumber.e;
+
+        let formattedExponent = format(exponent);
+
+        return dcNumber.m.toFixed(2) + "e" + formattedExponent;
+    }
+}
--- test.js
+++ test.js
@@ -3,5 +3,5 @@ let antimatter = new Decimal(10);
 let infinityTest = new Decimal("1e1000000");
 
 alert("--- BREAK_INFINITY.JS TEST ---");
-alert("ANTIMATTER:", antimatter.toString());
-alert("COSMIC NUMBER:", infinityTest.toString());
+alert("ANTIMATTER: " + antimatter.toString());
+alert("COSMIC NUMBER: " + infinityTest.toString());
