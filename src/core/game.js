0a568f9ffce7b78145aec1acecd4ad57a923b3f2
SomeRandomADForker <13ivsmir@gmail.com>
2026-07-09 07:36:14 +0000

    First ever UX

--- src/core/game.js
+++ src/core/game.js
@@ -1,4 +1,17 @@
-let antimatter = new Decimal("1.00e1234567890");
+let score = new Decimal(0);
 
-console.log("--- FORMAT TEST ---");
-console.log("FORMATTED VERY BIG NUMBER:\n" + format(antimatter));
+function updateUI() {
+    const scoreDisplay = document.getElementById("scoreDisplay");
+
+    scoreDisplay.innerText = "You have " + format(score) + " Score";
+}
+
+function increment() {
+    score = score.plus(1);
+
+    updateUI();
+}
+
+setInterval(() => {
+    updateUI();
+}, 50);
