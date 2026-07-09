80c7cbe8f3343b2f1abdba9ca42512de9ad0e0f5
SomeRandomADForker <13ivsmir@gmail.com>
2026-07-09 07:00:52 +0000

    Fix incorrect formatting

--- format.js
+++ format.js
@@ -1,7 +1,8 @@
 /**
  * @param {import("./index").default} dcNumber
+ * @param {boolean} isRecursion
  */
-function format(dcNumber) {
+function format(dcNumber, isRecursion = false) {
     if (!(dcNumber instanceof Decimal)) {
         dcNumber = new Decimal(dcNumber);
     }
@@ -12,13 +13,13 @@ function format(dcNumber) {
     }
     // > 1.00e6 (1,000,000) and < 1.00e1,000,000,000 (1.00e1.000e9)
     else if (dcNumber.gte(new Decimal("1e6")) && dcNumber.lt(new Decimal("1e1000000000"))) {
-        return dcNumber.toExponential(2).replace(/\+/g, "");
+        return dcNumber.toExponential(isRecursion ? 3 : 2).replace(/\+/g, "");
     }
     // > 1.00e1.000e9
     else {
         let exponent = dcNumber.e;
 
-        let formattedExponent = format(exponent);
+        let formattedExponent = format(exponent, true);
 
         return dcNumber.m.toFixed(2) + "e" + formattedExponent;
     }
