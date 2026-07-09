3db6582fd454722ccb4e986ed728cd2d40ddcdbe
SomeRandomADForker <13ivsmir@gmail.com>
2026-07-09 05:54:29 +0000

    Add real break_infinity.js library (that one with jsdelivr didn't work)

---
+++ test.js
@@ -0,0 +1,7 @@
+let antimatter = new Decimal(10);
+
+let infinityTest = new Decimal("1e1000000");
+
+alert("--- BREAK_INFINITY.JS TEST ---");
+alert("ANTIMATTER:", antimatter.toString());
+alert("COSMIC NUMBER:", infinityTest.toString());
